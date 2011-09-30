0924fa119c8cafee22df74704ad9231de7a78519
Paul <sparksco@gmail.com>
2011-09-29 22:34:09 -0600

    This build is meant as a stock CM Kernel build

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 9 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#9 PREEMPT Thu Sep 29 05:06:42 MDT 2011"
+#define LINUX_COMPILE_TIME "05:06:42"
+#define LINUX_COMPILE_BY "seth80"
+#define LINUX_COMPILE_HOST "SparksCo-Work-Bot"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
